02d46682d6e71c8ff8366387fc6eea7c518607bf
Phan Tan Dung <phantandung92@gmail.com>
2016-09-22 12:52:08 +0700

    Hướng dẫn cập nhật

---
+++ README.md
@@ -0,0 +1,52 @@
+# Hướng dẫn cập nhật module download từ 4.0.29 lên 4.1.00
+
+Chú ý: 
+- Gói cập nhật này chỉ dành cho module download 4.0.29, nếu module của bạn không ở phiên bản này cần tìm các hướng dẫn cập nhật lên 4.0.29 trước.
+- Module download 4.1.00 hoạt động tốt trên NukeViet 4 RC3, NukeViet 4 Official và NukeViet 4.1 Beta
+
+## Chuẩn bị cập nhật
+
+Backup toàn bộ CSDL dữ liệu và code của site đề phòng rủi ro.
+
+## Thực hiện cập nhật
+
+Đăng nhập quản trị site, di chuyển vào khu vực Công cụ web => Kiểm tra phiên bản, tại đây nếu hệ thống kiểm tra được module download và có yêu cầu cập nhật hãy tiến hành theo hướng dẫn của hệ thống.
+
+Nếu không cập nhật được theo cách trên hãy thực hiện cập nhật thủ công như sau:
+
+Tải gói cập nhật tại https://github.com/nukeviet/module-download/releases/download/to-4.1.00/update-to-4.1.00.zip. Giải nén và upload thư mục install lên ngang hàng với thư mục install trên server. Đăng nhập quản trị site, nhận được thông báo cập nhật và tiến hành cập nhật theo hướng dẫn của hệ thống.
+
+## Xử lý sau cập nhật
+
+Cần chú ý một số công việc sau:
+
+- Vào mục cấu hình module để thiết lập chức năng share qua cổng AddThis, chức năng xử lý phần xem trực tuyến.
+- Nếu giao diện của bạn sử dụng không phải giao diện default cần thực hiện các công việc bên dưới
+
+## Cập nhật giao diện
+
+Nếu tồn tại file `themes/ten-theme/modules/download/theme.php`, mở nó, tìm vị trí hàm `view_file`. Bên trong hàm thêm vào vị trí thích hợp đoạn
+
+```php
+    if ($download_config['shareport'] == 'addthis') {
+        $xtpl->assign('ADDTHIS_PUBID', $download_config['addthis_pubid']);
+        $xtpl->parse('main.addthis');
+    }
+```
+
+Nếu tồn tại file `themes/ten-theme/modules/download/viewfile.tpl`, mở nó tìm 
+
+```html
+<h2 class="m-bottom">{ROW.title}</h2>
+```
+
+Hoặc tương đương thêm vào sau
+
+```html
+    <!-- BEGIN: addthis -->
+    <div class="m-bottom clearfix">
+        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid={ADDTHIS_PUBID}"></script>
+        <div class="addthis_sharing_toolbox"></div>
+    </div>
+    <!-- END: addthis -->
+```
--- install/update_data.php
+++ install/update_data.php
@@ -17,20 +17,17 @@ $nv_update_config = array();
 $nv_update_config['type'] = 1;
 
 // ID goi cap nhat
-$nv_update_config['packageID'] = 'NVUDDOWNLOAD4029';
+$nv_update_config['packageID'] = 'NVUDDOWNLOAD4100';
 
 // Cap nhat cho module nao, de trong neu la cap nhat NukeViet, ten thu muc module neu la cap nhat module
 $nv_update_config['formodule'] = 'download';
 
 // Thong tin phien ban, tac gia, ho tro
-$nv_update_config['release_date'] = 1463590800;
+$nv_update_config['release_date'] = 1474563600;
 $nv_update_config['author'] = 'VINADES.,JSC (contact@vinades.vn)';
-$nv_update_config['support_website'] = 'https://github.com/nukeviet/module-download/tree/4.0.29';
-$nv_update_config['to_version'] = '4.0.29';
-$nv_update_config['allow_old_version'] = array(
-    '4.0.25',
-    '4.0.27'
-);
+$nv_update_config['support_website'] = 'https://github.com/nukeviet/module-download/tree/to-4.1.00';
+$nv_update_config['to_version'] = '4.1.00';
+$nv_update_config['allow_old_version'] = array('4.0.29');
 
 // 0:Nang cap bang tay, 1:Nang cap tu dong, 2:Nang cap nua tu dong
 $nv_update_config['update_auto_type'] = 1;
@@ -39,32 +36,25 @@ $nv_update_config['lang'] = array();
 $nv_update_config['lang']['vi'] = array();
 
 // Tiếng Việt
-$nv_update_config['lang']['vi']['nv_up_addtable'] = 'Thêm các bảng dữ liệu mới';
-$nv_update_config['lang']['vi']['nv_up_copydata'] = 'Chuyển dữ liệu sang bảng mới';
-$nv_update_config['lang']['vi']['nv_up_structre'] = 'Thay đổi cấu trúc dữ liệu các bảng cũ';
+$nv_update_config['lang']['vi']['nv_up_addthis'] = 'Thêm chức năng chia sẻ';
+$nv_update_config['lang']['vi']['nv_up_onlineview'] = 'Thêm chức năng xử lý xem trực tuyến';
 $nv_update_config['lang']['vi']['nv_up_finish'] = 'Đánh dấu phiên bản mới';
 
 $nv_update_config['tasklist'] = array();
 $nv_update_config['tasklist'][] = array(
-    'r' => '4.0.29',
+    'r' => '4.1.00',
     'rq' => 1,
-    'l' => 'nv_up_addtable',
-    'f' => 'nv_up_addtable'
+    'l' => 'nv_up_addthis',
+    'f' => 'nv_up_addthis'
 );
 $nv_update_config['tasklist'][] = array(
-    'r' => '4.0.29',
-    'rq' => 2,
-    'l' => 'nv_up_copydata',
-    'f' => 'nv_up_copydata'
-);
-$nv_update_config['tasklist'][] = array(
-    'r' => '4.0.29',
+    'r' => '4.1.00',
     'rq' => 1,
-    'l' => 'nv_up_structre',
-    'f' => 'nv_up_structre'
+    'l' => 'nv_up_onlineview',
+    'f' => 'nv_up_onlineview'
 );
 $nv_update_config['tasklist'][] = array(
-    'r' => '4.0.29',
+    'r' => '4.1.00',
     'rq' => 1,
     'l' => 'nv_up_finish',
     'f' => 'nv_up_finish'
@@ -120,12 +110,12 @@ while (list($_tmp) = $result->fetch(PDO::FETCH_NUM)) {
 }
 
 /**
- * nv_up_addtable()
+ * nv_up_addthis()
  *
  * @return
  *
  */
-function nv_up_addtable()
+function nv_up_addthis()
 {
     global $nv_update_baseurl, $db, $db_config, $nv_Cache, $array_modlang_update;
 
@@ -141,66 +131,8 @@ function nv_up_addtable()
     foreach ($array_modlang_update as $lang => $array_mod) {
         foreach ($array_mod['mod'] as $module_info) {
             $table_prefix = $db_config['prefix'] . "_" . $lang . "_" . $module_info['module_data'];
-            
-            // Thêm bảng fileupload
             try {
-                $db->query("CREATE TABLE IF NOT EXISTS " . $table_prefix . "_files (
-                    file_id int(11) unsigned NOT NULL AUTO_INCREMENT,
-                    download_id int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'ID file download',
-                    server_id smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT 'ID fileserver hoặc 0 nếu là local',
-                    file_path varchar(255) NOT NULL DEFAULT '',
-                    scorm_path varchar(255) NOT NULL DEFAULT '',
-                    filesize int(11) NOT NULL DEFAULT '0',
-                    weight smallint(4) unsigned NOT NULL DEFAULT '0',
-                    status tinyint(1) unsigned NOT NULL DEFAULT '0',
-                    PRIMARY KEY (file_id),
-                    KEY download_id (download_id),
-                    KEY server_id (server_id)
-                )ENGINE=MyISAM");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            
-            // Thêm bảng uploadserver
-            try {
-                $db->query("CREATE TABLE " . $table_prefix . "_server (
-                    server_id smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-                    server_name varchar(250) NOT NULL DEFAULT '',
-                    upload_url varchar(255) NOT NULL DEFAULT '',
-                    access_key varchar(255) NOT NULL DEFAULT '',
-                    secret_key varchar(255) NOT NULL DEFAULT '',
-                    status tinyint(1) unsigned NOT NULL DEFAULT '0',
-                    UNIQUE KEY server_name (server_name),
-                    PRIMARY KEY (server_id)
-                )ENGINE=MyISAM");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            
-            // Thêm bảng detail
-            try {
-                $db->query("CREATE TABLE IF NOT EXISTS " . $table_prefix . "_detail (
-                    id int(11) unsigned NOT NULL,
-                    description mediumtext NOT NULL,
-                    linkdirect text NOT NULL,
-                    groups_comment varchar(255) NOT NULL,
-                    groups_view varchar(255) NOT NULL,
-                    groups_onlineview varchar(255) NOT NULL,
-                    groups_download varchar(255) NOT NULL,
-                    rating_detail varchar(255) NOT NULL,
-                    PRIMARY KEY (id)
-                )ENGINE=MyISAM");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            
-            // Thêm hết các trường vào bảng rows
-            try {
-                $db->query("ALTER TABLE " . $table_prefix . " 
-                    ADD groups_onlineview VARCHAR(255) NOT NULL DEFAULT '' AFTER groups_view, 
-                    ADD num_fileupload SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0' AFTER copyright, 
-                    ADD num_linkdirect SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0' AFTER num_fileupload
-                ");
+                $db->query("INSERT INTO " . $table_prefix . "_config (config_name, config_value) VALUES ('shareport', 'none'), ('addthis_pubid', '');");
             } catch (PDOException $e) {
                 trigger_error($e->getMessage());
             }
@@ -211,140 +143,12 @@ function nv_up_addtable()
 }
 
 /**
- * nv_up_copydata()
+ * nv_up_onlineview()
  *
  * @return
  *
  */
-function nv_up_copydata()
-{
-    global $nv_update_baseurl, $db, $db_config, $nv_Cache, $array_modlang_update, $nv_Request, $array_modtable_update;
-
-    $return = array(
-        'status' => 1,
-        'complete' => 1,
-        'next' => 1,
-        'link' => 'NO',
-        'lang' => 'NO',
-        'message' => ''
-    );
-
-    $request = array();
-    $request['umodkey'] = $nv_Request->get_int('umodkey', 'get,post', 0); // Cập nhật cho module nào
-    $request['uoffset'] = $nv_Request->get_int('uoffset', 'get,post', 0); // Vị trí chạy tiếp theo
-    
-    // Kết thúc quá trình nếu hết bảng update
-    if (!isset($array_modtable_update[$request['umodkey']])) {
-        return $return;
-    }
-    
-    $return['complete'] = 0;
-    $return['next'] = 0;
-    
-    $table_prefix = $array_modtable_update[$request['umodkey']];
-    $limit_rows = 5000;
-    
-    $db->sqlreset()->select("id, description, fileupload, linkdirect, groups_comment, groups_view, groups_onlineview, groups_download, rating_detail")->from($table_prefix)->order("id ASC")->limit($limit_rows)->offset($request['uoffset']);
-    $sql = $db->sql();
-    $result = $db->query($sql);
-    $num_rows = $result->rowCount();
-    
-    while ($row = $result->fetch()) {
-        // Tính lại thống kê
-        $num_fileupload = 0;
-        $num_linkdirect = 0;
-
-        // Copy fileupload
-        $fileupload = explode('[NV]', $row['fileupload']);
-        $weight = 1;
-        
-        foreach ($fileupload as $file) {
-            if (! empty($file)) {
-                $file2 = NV_UPLOADS_DIR . $file;
-                
-                if (file_exists(NV_ROOTDIR . '/' . $file2) and ($filesize = filesize(NV_ROOTDIR . '/' . $file2)) != 0) {
-                    $num_fileupload ++;
-                    
-                    $sql = 'INSERT INTO ' . $table_prefix . '_files (
-                        download_id, server_id, file_path, scorm_path, filesize, weight, status
-                    ) VALUES (
-                        ' . $row['id'] . ', 0, :file_path, :scorm_path, :filesize, ' . ($weight++) . ', 1
-                    )';
-                    $data_insert = array();
-                    $data_insert['file_path'] = $file;
-                    $data_insert['scorm_path'] = '';
-                    $data_insert['filesize'] = $filesize;
-                    $file_id = $db->insert_id($sql, 'file_id', $data_insert);
-                    
-                    if (!$file_id) {
-                        $return['status'] = 0;
-                        return $return;
-                    }
-                }
-            }
-        }
-        
-        // Kiểm tra linkredirect
-        $linkdirect = explode('[NV]', $row['linkdirect']);
-        foreach ($linkdirect as $links) {
-            $links = array_filter(explode('<br />', $links));
-            $num_linkdirect += sizeof($links);
-        }
-        
-        // Copy detail
-        if (empty($row['groups_onlineview'])) {
-            $row['groups_onlineview'] = $row['groups_download'];
-        }
-        
-        try {
-            $stmt = $db->prepare("INSERT INTO " . $table_prefix . "_detail (
-                id, description, linkdirect, groups_comment, groups_view, groups_onlineview, groups_download, rating_detail
-            ) VALUES( 
-                " . $row['id'] . ", :description, :linkdirect, :groups_comment, :groups_view, :groups_onlineview, :groups_download, :rating_detail
-            )");
-            
-            $stmt->bindParam(':description', $row['description'], PDO::PARAM_STR, strlen($row['description']));
-            $stmt->bindParam(':linkdirect', $row['linkdirect'], PDO::PARAM_STR, strlen($row['linkdirect']));
-            $stmt->bindParam(':groups_comment', $row['groups_comment'], PDO::PARAM_STR);
-            $stmt->bindParam(':groups_view', $row['groups_view'], PDO::PARAM_STR);
-            $stmt->bindParam(':groups_onlineview', $row['groups_onlineview'], PDO::PARAM_STR);
-            $stmt->bindParam(':groups_download', $row['groups_download'], PDO::PARAM_STR);
-            $stmt->bindParam(':rating_detail', $row['rating_detail'], PDO::PARAM_STR);
-            $stmt->execute();
-        } catch (PDOException $e) {
-            trigger_error($e->getMessage());
-            $return['status'] = 0;
-            return $return;
-        }
-        
-        // Cập nhật lại thông tin
-        try {
-            $db->query("UPDATE " . $table_prefix . " SET num_fileupload=" . $num_fileupload . ", num_linkdirect=" . $num_linkdirect . " WHERE id=" . $row['id']);
-        } catch (PDOException $e) {
-            // Nothing
-        }
-    }
-    
-    if ($num_rows < $limit_rows) {
-        $request['uoffset'] = 0;
-        $request['umodkey'] ++;
-    } else {
-        // Tiếp tục
-        $request['uoffset'] += $limit_rows;
-    }
-    
-    $return['link'] = $nv_update_baseurl . '&umodkey=' . $request['umodkey'] . '&uoffset=' . $request['uoffset'];
-    return $return;
-}
-
-
-/**
- * nv_up_structre()
- *
- * @return
- *
- */
-function nv_up_structre()
+function nv_up_onlineview()
 {
     global $nv_update_baseurl, $db, $db_config, $nv_Cache, $array_modlang_update;
 
@@ -360,73 +164,8 @@ function nv_up_structre()
     foreach ($array_modlang_update as $lang => $array_mod) {
         foreach ($array_mod['mod'] as $module_info) {
             $table_prefix = $db_config['prefix'] . "_" . $lang . "_" . $module_info['module_data'];
-            
-            // Chỉnh sửa bảng categories
             try {
-                $db->query("ALTER TABLE " . $table_prefix . "_categories ADD groups_onlineview VARCHAR(255) NOT NULL DEFAULT '' AFTER groups_view");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            
-            // Cập nhật lại quyền xem trực tuyến
-            try {
-                $db->query("UPDATE " . $table_prefix . "_categories SET groups_onlineview=groups_download");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            
-            // Thêm một số cấu hình module
-            try {
-                $db->query("INSERT INTO " . $table_prefix . "_config VALUES
-                ('delfile_mode', '0'),
-                ('structure_upload', 'Ym'),
-                ('scorm_handle_mode', '0'),
-                ('fileserver', 0)");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            
-            // Xóa bỏ các trưởng bảng rows
-            try {
-                $db->query("ALTER TABLE " . $table_prefix . " 
-                  DROP fileupload,
-                  DROP description,
-                  DROP linkdirect,
-                  DROP groups_comment,
-                  DROP groups_view,
-                  DROP groups_onlineview,
-                  DROP groups_download,
-                  DROP rating_detail
-                ");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            
-            // Mở rộng giới hạn các trường
-            try {
-                $db->query("ALTER TABLE " . $table_prefix . " CHANGE id id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            try {
-                $db->query("ALTER TABLE " . $table_prefix . "_report CHANGE fid fid INT(11) UNSIGNED NOT NULL DEFAULT '0'");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            try {
-                $db->query("ALTER TABLE " . $table_prefix . "_tags CHANGE did did INT(11) UNSIGNED NOT NULL AUTO_INCREMENT");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            try {
-                $db->query("ALTER TABLE " . $table_prefix . "_tags_id CHANGE did did INT(11) UNSIGNED NOT NULL DEFAULT '0'");
-            } catch (PDOException $e) {
-                trigger_error($e->getMessage());
-            }
-            
-            // Đánh index
-            try {
-                $db->query("ALTER TABLE " . $table_prefix . " ADD INDEX title (title), ADD INDEX status (status), ADD INDEX uploadtime (uploadtime), ADD INDEX updatetime (updatetime)");
+                $db->query("INSERT INTO " . $table_prefix . "_config (config_name, config_value) VALUES ('pdf_handler', 'filetmp');");
             } catch (PDOException $e) {
                 trigger_error($e->getMessage());
             }
@@ -455,18 +194,17 @@ function nv_up_finish()
         'message' => ''
     );
 
-    @nv_deletefile(NV_ROOTDIR . '/modules/download/admin/add.php');
-    @nv_deletefile(NV_ROOTDIR . '/themes/admin_default/modules/download/filequeue_edit.tpl');
+    @nv_deletefile(NV_ROOTDIR . '/themes/default/js/pdf.js', true);
 
     try {
         $num = $db->query("SELECT COUNT(*) FROM " . $db_config['prefix'] . "_setup_extensions WHERE basename='" . $nv_update_config['formodule'] . "' AND type='module'")->fetchColumn();
-        $version = "4.0.29 1463590800";
+        $version = "4.1.00 " . $nv_update_config['release_date'];
         
         if (!$num) {
             $db->query("INSERT INTO " . $db_config['prefix'] . "_setup_extensions (
                 id, type, title, is_sys, is_virtual, basename, table_prefix, version, addtime, author, note
             ) VALUES (
-                25, 'module', 'download', 0, 1, 'download', 'download', '4.0.29 1463590800', " . NV_CURRENTTIME . ", 'VINADES.,JSC (contact@vinades.vn)', 
+                25, 'module', 'download', 0, 1, 'download', 'download', '4.1.00 " . $nv_update_config['release_date'] . "', " . NV_CURRENTTIME . ", 'VINADES.,JSC (contact@vinades.vn)', 
                 'Module download for NukeViet'
             )");
         } else {
